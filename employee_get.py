fa9d9f6e7cb341f5bc3431d04fdbd39eeaeb0d9a
handyman90 <helmi_052000@yahoo.com>
2024-10-16 16:54:31 +0800

    Update employee_get.py

--- employee_get.py
+++ employee_get.py
@@ -1,27 +1,23 @@
 from fastapi import FastAPI, HTTPException, Header, Depends
 import requests
-from pydantic import BaseModel
-from typing import Optional, Dict, Any, List
 from sqlalchemy.orm import Session
-from models import Employee, SessionLocal
+from models import SessionLocal, Employee
+from employee_get_models import EmployeeResponse
 from datetime import datetime
 
 app = FastAPI()
 
-# Token URL and payload for authentication
-token_url = "https://csmstg.censof.com/2023R1Preprod/identity/connect/token"
-
 # Function to authenticate and get a session token
 def get_auth_token() -> dict:
+    token_url = "https://csmstg.censof.com/2023R1Preprod/identity/connect/token"
     payload = {
         "grant_type": "password",
         "client_id": "03407458-3136-511B-24FB-68D470104D22@MIROS 090624",
-        "client_secret": "sa%40121314",
+        "client_secret": "3gVM0RbnqDwXYfO1aekAyw",
         "scope": "api",
         "username": "apiuser",
         "password": "apiuser"
     }
-
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
     response = requests.post(token_url, data=payload, headers=headers)
 
@@ -38,100 +34,6 @@ def get_db():
     finally:
         db.close()
 
-# Pydantic models for response structure
-class ValueField(BaseModel):
-    value: Optional[str]
-
-class Address(BaseModel):
-    id: Optional[str]
-    rowNumber: Optional[int]
-    note: Optional[str]
-    AddressLine1: ValueField
-    AddressLine2: ValueField
-    City: Optional[Dict[str, Any]]
-    Country: ValueField
-    PostalCode: Optional[Dict[str, Any]]
-    State: Optional[Dict[str, Any]]
-    custom: Optional[Dict[str, Any]]
-    files: Optional[List]
-
-class Contact(BaseModel):
-    id: Optional[str]
-    rowNumber: Optional[int]
-    note: Optional[str]
-    Address: Address
-    DisplayName: ValueField
-    Email: ValueField
-    Fax: Optional[Dict[str, Any]]
-    FirstName: Optional[Dict[str, Any]]
-    LastName: ValueField
-    MiddleName: Optional[Dict[str, Any]]
-    Phone1: Optional[Dict[str, Any]]
-    Phone1Type: ValueField
-    Phone2: Optional[Dict[str, Any]]
-    Phone2Type: ValueField
-    Title: ValueField
-    custom: Optional[Dict[str, Any]]
-    files: Optional[List]
-
-class EmploymentHistory(BaseModel):
-    id: Optional[str]
-    rowNumber: Optional[int]
-    note: Optional[str]
-    Active: ValueField
-    EndDate: Optional[Dict[str, Any]]
-    LineNbr: ValueField
-    PositionID: ValueField
-    RehireEligible: ValueField
-    StartDate: ValueField
-    StartReason: ValueField
-    Terminated: ValueField
-    TerminationReason: Optional[Dict[str, Any]]
-    custom: Optional[Dict[str, Any]]
-    files: Optional[List]
-
-class PaymentInstruction(BaseModel):
-    id: Optional[str]
-    rowNumber: Optional[int]
-    note: Optional[str]
-    BAccountID: ValueField
-    Description: ValueField
-    InstructionID: ValueField
-    LocationID: ValueField
-    PaymentMethod: ValueField
-    Value: ValueField
-    custom: Optional[Dict[str, Any]]
-    files: Optional[List]
-
-class EmployeeResponse(BaseModel):
-    id: str
-    rowNumber: Optional[int] = None
-    note: Optional[str] = None
-    BranchID: ValueField
-    Calendar: ValueField
-    CashAccount: ValueField
-    Contact: Contact
-    CurrencyID: ValueField
-    DateOfBirth: ValueField
-    DepartmentID: ValueField
-    EmployeeClassID: ValueField
-    EmployeeID: ValueField
-    EmploymentHistory: List[EmploymentHistory]
-    ExpenseAccount: ValueField
-    ExpenseSubaccount: ValueField
-    IdentityNumber: ValueField
-    IdentityType: ValueField
-    LastModifiedDateTime: ValueField
-    Name: ValueField
-    PaymentInstruction: List[PaymentInstruction]
-    PaymentMethod: ValueField
-    ReportsToID: Optional[Dict[str, Any]] = None
-    SalesAccount: ValueField
-    SalesSubaccount: ValueField
-    Status: ValueField
-    custom: Optional[Dict[str, Any]] = None
-    links: Optional[Dict[str, Any]] = None
-
 # Endpoint to retrieve employee information
 @app.get("/organization/employee/{employee_id}", response_model=EmployeeResponse)
 def get_employee(employee_id: str, authorization: str = Header(None), db: Session = Depends(get_db)):
@@ -146,7 +48,6 @@ def get_employee(employee_id: str, authorization: str = Header(None), db: Sessio
 
     if response.status_code == 200:
         employee_data = response.json()
-        
         return EmployeeResponse(**employee_data)
 
     raise HTTPException(status_code=response.status_code, detail="Error fetching employee data")
@@ -154,4 +55,4 @@ def get_employee(employee_id: str, authorization: str = Header(None), db: Sessio
 # Run the app
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)  # Set to 0.0.0.0 to accept requests from any IP
+    uvicorn.run(app, host="0.0.0.0", port=8000)
