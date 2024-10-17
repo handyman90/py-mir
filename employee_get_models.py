8badc86eed66ca0a894f3832ad7e1a053b4c75e3
handyman90 <helmi_052000@yahoo.com>
2024-10-17 15:25:03 +0800

    Update employee_get_models.py

--- employee_get_models.py
+++ employee_get_models.py
@@ -1,71 +1,70 @@
 from pydantic import BaseModel
-from typing import Optional, List, Dict
+from typing import Optional, List, Dict, Any
 
 class ValueField(BaseModel):
-    value: Optional[str]  # Make value optional
+    value: Optional[str]
 
 class Address(BaseModel):
-    id: Optional[str]  # Made optional
-    rowNumber: Optional[int] = None
-    note: Optional[str] = None
-    AddressLine1: ValueField = None
-    AddressLine2: ValueField = None
-    City: Optional[Dict] = None
-    Country: ValueField = None
-    PostalCode: Optional[Dict] = None
-    State: Optional[Dict] = None
-    custom: Optional[Dict] = None
-    files: Optional[List] = None
+    id: Optional[str]
+    rowNumber: Optional[int]
+    note: Optional[str]
+    AddressLine1: ValueField
+    AddressLine2: ValueField
+    City: Optional[Dict]
+    Country: ValueField
+    PostalCode: Optional[Dict]
+    State: Optional[Dict]
+    custom: Optional[Dict]
+    files: List[Dict]
 
 class Contact(BaseModel):
-    id: Optional[str]  # Made optional
-    rowNumber: Optional[int] = None
-    note: Optional[str] = None
-    Address: Address = None
-    DisplayName: ValueField = None
-    Email: ValueField = None
-    Fax: Optional[Dict] = None
-    FirstName: Optional[Dict] = None
-    LastName: ValueField = None
-    MiddleName: Optional[Dict] = None
-    Phone1: Optional[Dict] = None
-    Phone1Type: ValueField = None
-    Phone2: Optional[Dict] = None
-    Phone2Type: ValueField = None
-    Title: ValueField = None
-    WebSite: Optional[Dict] = None
-    custom: Optional[Dict] = None
-    files: Optional[List] = None
+    id: Optional[str]
+    rowNumber: Optional[int]
+    note: Optional[str]
+    Address: Address
+    DisplayName: ValueField
+    Email: ValueField
+    Fax: Optional[Dict]
+    FirstName: Optional[Dict]
+    LastName: ValueField
+    MiddleName: Optional[Dict]
+    Phone1: Optional[Dict]
+    Phone1Type: ValueField
+    Phone2: Optional[Dict]
+    Phone2Type: ValueField
+    Title: ValueField
+    WebSite: Optional[Dict]
+    custom: Optional[Dict]
+    files: List[Dict]
 
 class EmploymentHistory(BaseModel):
-    id: Optional[str]  # Made optional
-    rowNumber: Optional[int] = None
-    note: Optional[str] = None
-    Active: ValueField = None
-    EndDate: Optional[Dict] = None
-    LineNbr: ValueField = None
-    PositionID: ValueField = None
-    RehireEligible: ValueField = None
-    StartDate: ValueField = None
-    StartReason: ValueField = None
-    Terminated: ValueField = None
-    TerminationReason: Optional[Dict] = None
-    custom: Optional[Dict] = None
-    _links: Optional[Dict] = None
-    files: Optional[List] = None
+    id: Optional[str]
+    rowNumber: Optional[int]
+    note: Optional[str]
+    Active: ValueField
+    EndDate: Optional[Dict]
+    LineNbr: ValueField
+    PositionID: ValueField
+    RehireEligible: ValueField
+    StartDate: ValueField
+    StartReason: ValueField
+    Terminated: ValueField
+    TerminationReason: Optional[Dict]
+    custom: Optional[Dict]
+    files: List[Dict]
 
 class PaymentInstruction(BaseModel):
-    id: Optional[str]  # Made optional
-    rowNumber: Optional[int] = None
-    note: Optional[str] = None
-    BAccountID: ValueField = None
-    Description: ValueField = None
-    InstructionID: ValueField = None
-    LocationID: ValueField = None
-    PaymentMethod: ValueField = None
-    Value: ValueField = None
-    custom: Optional[Dict] = None
-    files: Optional[List] = None
+    id: Optional[str]
+    rowNumber: Optional[int]
+    note: Optional[str]
+    BAccountID: ValueField
+    Description: ValueField
+    InstructionID: ValueField
+    LocationID: ValueField
+    PaymentMethod: ValueField
+    Value: ValueField
+    custom: Optional[Dict]
+    files: List[Dict]
 
 class EmployeeResponse(BaseModel):
     id: str
@@ -88,10 +87,10 @@ class EmployeeResponse(BaseModel):
     LastModifiedDateTime: ValueField
     Name: ValueField
     PaymentInstruction: List[PaymentInstruction]
-    PaymentMethod: ValueField
-    ReportsToID: Optional[Dict] = None
+    ReportsToID: Optional[Dict]
     SalesAccount: ValueField
     SalesSubaccount: ValueField
     Status: ValueField
-    custom: Optional[Dict] = None
-    links: Optional[Dict] = None
+    custom: Optional[Dict]
+    _links: Optional[Dict]
+    files: List[Dict]
