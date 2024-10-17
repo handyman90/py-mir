d02bd0b75f73fa9aaabdcd1f2c616d0a5d5470b9
handyman90 <helmi_052000@yahoo.com>
2024-10-17 15:15:23 +0800

    Update employee_get_models.py

--- employee_get_models.py
+++ employee_get_models.py
@@ -1,64 +1,71 @@
 from pydantic import BaseModel
-from typing import Optional, List, Dict, Any
+from typing import Optional, List, Dict
 
 class ValueField(BaseModel):
     value: Optional[str]  # Make value optional
 
-class Link(BaseModel):
-    self: Optional[str]
-    files_put: Optional[str]
+class Address(BaseModel):
+    id: Optional[str]  # Made optional
+    rowNumber: Optional[int] = None
+    note: Optional[str] = None
+    AddressLine1: ValueField = None
+    AddressLine2: ValueField = None
+    City: Optional[Dict] = None
+    Country: ValueField = None
+    PostalCode: Optional[Dict] = None
+    State: Optional[Dict] = None
+    custom: Optional[Dict] = None
+    files: Optional[List] = None
 
 class Contact(BaseModel):
-    id: str
+    id: Optional[str]  # Made optional
     rowNumber: Optional[int] = None
     note: Optional[str] = None
-    DisplayName: ValueField
-    Email: ValueField
-    Fax: ValueField
-    FirstName: ValueField
-    LastName: ValueField
-    MiddleName: ValueField
-    Phone1: ValueField
-    Phone1Type: ValueField
-    Phone2: ValueField
-    Phone2Type: ValueField
-    Title: ValueField
-
-class Address(BaseModel):
-    id: str
-    rowNumber: Optional[int] = None
-    note: Optional[str] = None
-    AddressLine1: ValueField
-    AddressLine2: ValueField
-    City: ValueField
-    Country: ValueField
-    PostalCode: ValueField
-    State: ValueField
+    Address: Address = None
+    DisplayName: ValueField = None
+    Email: ValueField = None
+    Fax: Optional[Dict] = None
+    FirstName: Optional[Dict] = None
+    LastName: ValueField = None
+    MiddleName: Optional[Dict] = None
+    Phone1: Optional[Dict] = None
+    Phone1Type: ValueField = None
+    Phone2: Optional[Dict] = None
+    Phone2Type: ValueField = None
+    Title: ValueField = None
+    WebSite: Optional[Dict] = None
+    custom: Optional[Dict] = None
+    files: Optional[List] = None
 
 class EmploymentHistory(BaseModel):
-    id: str
+    id: Optional[str]  # Made optional
     rowNumber: Optional[int] = None
     note: Optional[str] = None
-    Active: ValueField
-    EndDate: Optional[Dict[str, Any]] = None
-    LineNbr: ValueField
-    PositionID: ValueField
-    RehireEligible: ValueField
-    StartDate: ValueField
-    StartReason: ValueField
-    Terminated: ValueField
-    TerminationReason: Optional[Dict[str, Any]] = None
+    Active: ValueField = None
+    EndDate: Optional[Dict] = None
+    LineNbr: ValueField = None
+    PositionID: ValueField = None
+    RehireEligible: ValueField = None
+    StartDate: ValueField = None
+    StartReason: ValueField = None
+    Terminated: ValueField = None
+    TerminationReason: Optional[Dict] = None
+    custom: Optional[Dict] = None
+    _links: Optional[Dict] = None
+    files: Optional[List] = None
 
 class PaymentInstruction(BaseModel):
-    id: str
+    id: Optional[str]  # Made optional
     rowNumber: Optional[int] = None
     note: Optional[str] = None
-    BAccountID: ValueField
-    Description: ValueField
-    InstructionID: ValueField
-    LocationID: ValueField
-    PaymentMethod: ValueField
-    Value: ValueField
+    BAccountID: ValueField = None
+    Description: ValueField = None
+    InstructionID: ValueField = None
+    LocationID: ValueField = None
+    PaymentMethod: ValueField = None
+    Value: ValueField = None
+    custom: Optional[Dict] = None
+    files: Optional[List] = None
 
 class EmployeeResponse(BaseModel):
     id: str
@@ -82,9 +89,9 @@ class EmployeeResponse(BaseModel):
     Name: ValueField
     PaymentInstruction: List[PaymentInstruction]
     PaymentMethod: ValueField
-    ReportsToID: Optional[str] = None
+    ReportsToID: Optional[Dict] = None
     SalesAccount: ValueField
     SalesSubaccount: ValueField
     Status: ValueField
-    custom: Optional[Dict[str, Any]] = None
-    links: Link
+    custom: Optional[Dict] = None
+    links: Optional[Dict] = None
