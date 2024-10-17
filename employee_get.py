9cb1b4adc442ae6a1f86c616993d50888c267680
handyman90 <helmi_052000@yahoo.com>
2024-10-17 14:53:58 +0800

    Update employee_get.py

--- employee_get.py
+++ employee_get.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException, Header, Depends
 import requests
 from sqlalchemy.orm import Session
-from models import SessionLocal
+from models import SessionLocal, Employee
 from employee_get_models import EmployeeResponse
 from datetime import datetime
 from typing import Optional
@@ -38,7 +38,7 @@ def get_auth_token():
 # Endpoint to retrieve and save employee information
 @app.get("/organization/employee/{employee_id}", response_model=EmployeeResponse)
 def get_employee(employee_id: str, authorization: Optional[str] = Header(None), db: Session = Depends(get_db)):
-    if not authorization:
+    if authorization is None:
         token_response = get_auth_token()
         authorization = token_response.get("access_token")
 
@@ -52,78 +52,70 @@ def get_employee(employee_id: str, authorization: Optional[str] = Header(None), 
 
         # Flatten nested fields and create a new employee object or update an existing one
         employee = Employee(
-            id=employee_data.get("id"),
+            id=employee_data["id"],
             row_number=employee_data.get("rowNumber"),
-            note=employee_data.get("note", ""),
-            BranchID=employee_data.get("BranchID", {}).get("value"),
-            Calendar=employee_data.get("Calendar", {}).get("value"),
-            CashAccount=employee_data.get("CashAccount", {}).get("value"),
-            CurrencyID=employee_data.get("CurrencyID", {}).get("value"),
-            DateOfBirth=parse_date(employee_data.get("DateOfBirth", {}).get("value")),
-            DepartmentID=employee_data.get("DepartmentID", {}).get("value"),
-            EmployeeClassID=employee_data.get("EmployeeClassID", {}).get("value"),
-            EmployeeID=employee_data.get("EmployeeID", {}).get("value"),
-            ExpenseAccount=employee_data.get("ExpenseAccount", {}).get("value"),
-            ExpenseSubaccount=employee_data.get("ExpenseSubaccount", {}).get("value"),
-            IdentityNumber=employee_data.get("IdentityNumber", {}).get("value"),
-            IdentityType=employee_data.get("IdentityType", {}).get("value"),
-            LastModifiedDateTime=parse_date(employee_data.get("LastModifiedDateTime", {}).get("value")),
-            Name=employee_data.get("Name", {}).get("value"),
-            PaymentMethod=employee_data.get("PaymentMethod", {}).get("value"),
-            SalesAccount=employee_data.get("SalesAccount", {}).get("value"),
-            SalesSubaccount=employee_data.get("SalesSubaccount", {}).get("value"),
-            Status=employee_data.get("Status", {}).get("value"),
-            ContactID=employee_data.get("Contact", {}).get("id"),
-            ContactRowNumber=employee_data.get("Contact", {}).get("rowNumber"),
-            ContactNote=employee_data.get("Contact", {}).get("note"),
-            ContactDisplayName=employee_data.get("Contact", {}).get("DisplayName", {}).get("value"),
-            ContactEmail=employee_data.get("Contact", {}).get("Email", {}).get("value"),
-            ContactFax=employee_data.get("Contact", {}).get("Fax", {}).get("value"),
-            ContactFirstName=employee_data.get("Contact", {}).get("FirstName", {}).get("value"),
-            ContactLastName=employee_data.get("Contact", {}).get("LastName", {}).get("value"),
-            ContactMiddleName=employee_data.get("Contact", {}).get("MiddleName", {}).get("value"),
-            ContactPhone1=employee_data.get("Contact", {}).get("Phone1", {}).get("value"),
-            ContactPhone1Type=employee_data.get("Contact", {}).get("Phone1Type", {}).get("value"),
-            ContactPhone2=employee_data.get("Contact", {}).get("Phone2", {}).get("value"),
-            ContactPhone2Type=employee_data.get("Contact", {}).get("Phone2Type", {}).get("value"),
-            ContactTitle=employee_data.get("Contact", {}).get("Title", {}).get("value"),
-            AddressID=employee_data.get("Contact", {}).get("Address", {}).get("id"),
-            AddressRowNumber=employee_data.get("Contact", {}).get("Address", {}).get("rowNumber"),
-            AddressNote=employee_data.get("Contact", {}).get("Address", {}).get("note"),
-            AddressLine1=employee_data.get("Contact", {}).get("Address", {}).get("AddressLine1", {}).get("value"),
-            AddressLine2=employee_data.get("Contact", {}).get("Address", {}).get("AddressLine2", {}).get("value"),
-            AddressCity=employee_data.get("Contact", {}).get("Address", {}).get("City", {}).get("value"),
-            AddressCountry=employee_data.get("Contact", {}).get("Address", {}).get("Country", {}).get("value"),
-            AddressPostalCode=employee_data.get("Contact", {}).get("Address", {}).get("PostalCode", {}).get("value"),
-            AddressState=employee_data.get("Contact", {}).get("Address", {}).get("State", {}).get("value"),
-            EmploymentHistoryID=employee_data.get("EmploymentHistory", [{}])[0].get("id"),
-            EmploymentHistoryRowNumber=employee_data.get("EmploymentHistory", [{}])[0].get("rowNumber"),
-            EmploymentHistoryNote=employee_data.get("EmploymentHistory", [{}])[0].get("note"),
-            EmploymentHistoryActive=employee_data.get("EmploymentHistory", [{}])[0].get("Active", {}).get("value"),
-            EmploymentHistoryEndDate=parse_date(employee_data.get("EmploymentHistory", [{}])[0].get("EndDate", {}).get("value")),
-            EmploymentHistoryLineNbr=employee_data.get("EmploymentHistory", [{}])[0].get("LineNbr", {}).get("value"),
-            EmploymentHistoryPositionID=employee_data.get("EmploymentHistory", [{}])[0].get("PositionID", {}).get("value"),
-            EmploymentHistoryRehireEligible=employee_data.get("EmploymentHistory", [{}])[0].get("RehireEligible", {}).get("value"),
-            EmploymentHistoryStartDate=parse_date(employee_data.get("EmploymentHistory", [{}])[0].get("StartDate", {}).get("value")),
-            EmploymentHistoryStartReason=employee_data.get("EmploymentHistory", [{}])[0].get("StartReason", {}).get("value"),
-            EmploymentHistoryTerminated=employee_data.get("EmploymentHistory", [{}])[0].get("Terminated", {}).get("value"),
-            EmploymentHistoryTerminationReason=employee_data.get("EmploymentHistory", [{}])[0].get("TerminationReason", {}).get("value"),
-            PaymentInstructionID=employee_data.get("PaymentInstruction", [{}])[0].get("id"),
-            PaymentInstructionRowNumber=employee_data.get("PaymentInstruction", [{}])[0].get("rowNumber"),
-            PaymentInstructionNote=employee_data.get("PaymentInstruction", [{}])[0].get("note"),
-            PaymentInstructionBAccountID=employee_data.get("PaymentInstruction", [{}])[0].get("BAccountID", {}).get("value"),
-            PaymentInstructionDescription=employee_data.get("PaymentInstruction", [{}])[0].get("Description", {}).get("value"),
-            PaymentInstructionInstructionID=employee_data.get("PaymentInstruction", [{}])[0].get("InstructionID", {}).get("value"),
-            PaymentInstructionLocationID=employee_data.get("PaymentInstruction", [{}])[0].get("LocationID", {}).get("value"),
-            PaymentInstructionMethod=employee_data.get("PaymentInstruction", [{}])[0].get("PaymentMethod", {}).get("value"),
-            PaymentInstructionValue=employee_data.get("PaymentInstruction", [{}])[0].get("Value", {}).get("value")
+            note=employee_data.get("note"),
+            BranchID=employee_data["BranchID"]["value"],
+            Calendar=employee_data["Calendar"]["value"],
+            CashAccount=employee_data["CashAccount"]["value"],
+            ContactID=employee_data["Contact"]["id"],
+            ContactRowNumber=employee_data["Contact"]["rowNumber"],
+            ContactNote=employee_data["Contact"].get("note"),
+            ContactDisplayName=employee_data["Contact"]["DisplayName"]["value"],
+            ContactEmail=employee_data["Contact"]["Email"]["value"],
+            ContactFax=employee_data["Contact"].get("Fax"),
+            ContactFirstName=employee_data["Contact"].get("FirstName"),
+            ContactLastName=employee_data["Contact"]["LastName"]["value"],
+            ContactMiddleName=employee_data["Contact"].get("MiddleName"),
+            ContactPhone1=employee_data["Contact"].get("Phone1"),
+            ContactPhone1Type=employee_data["Contact"]["Phone1Type"]["value"],
+            ContactPhone2=employee_data["Contact"].get("Phone2"),
+            ContactPhone2Type=employee_data["Contact"]["Phone2Type"]["value"],
+            ContactTitle=employee_data["Contact"]["Title"]["value"],
+            CurrencyID=employee_data["CurrencyID"]["value"],
+            DateOfBirth=datetime.fromisoformat(employee_data["DateOfBirth"]["value"].replace("Z", "+00:00")),
+            DepartmentID=employee_data["DepartmentID"]["value"],
+            EmployeeClassID=employee_data["EmployeeClassID"]["value"],
+            EmployeeID=employee_data["EmployeeID"]["value"],
+            EmploymentHistoryID=employee_data["EmploymentHistory"][0]["id"] if employee_data["EmploymentHistory"] else None,
+            EmploymentHistoryRowNumber=employee_data["EmploymentHistory"][0]["rowNumber"] if employee_data["EmploymentHistory"] else None,
+            EmploymentHistoryNote=employee_data["EmploymentHistory"][0].get("note"),
+            EmploymentHistoryActive=employee_data["EmploymentHistory"][0]["Active"]["value"] if employee_data["EmploymentHistory"] else None,
+            EmploymentHistoryEndDate=employee_data["EmploymentHistory"][0].get("EndDate"),
+            EmploymentHistoryLineNbr=employee_data["EmploymentHistory"][0]["LineNbr"]["value"] if employee_data["EmploymentHistory"] else None,
+            EmploymentHistoryPositionID=employee_data["EmploymentHistory"][0]["PositionID"]["value"] if employee_data["EmploymentHistory"] else None,
+            EmploymentHistoryRehireEligible=employee_data["EmploymentHistory"][0]["RehireEligible"]["value"] if employee_data["EmploymentHistory"] else None,
+            EmploymentHistoryStartDate=employee_data["EmploymentHistory"][0]["StartDate"]["value"] if employee_data["EmploymentHistory"] else None,
+            EmploymentHistoryStartReason=employee_data["EmploymentHistory"][0]["StartReason"]["value"] if employee_data["EmploymentHistory"] else None,
+            EmploymentHistoryTerminated=employee_data["EmploymentHistory"][0]["Terminated"]["value"] if employee_data["EmploymentHistory"] else None,
+            EmploymentHistoryTerminationReason=employee_data["EmploymentHistory"][0].get("TerminationReason"),
+            ExpenseAccount=employee_data["ExpenseAccount"]["value"],
+            ExpenseSubaccount=employee_data["ExpenseSubaccount"]["value"],
+            IdentityNumber=employee_data["IdentityNumber"]["value"],
+            IdentityType=employee_data["IdentityType"]["value"],
+            LastModifiedDateTime=datetime.fromisoformat(employee_data["LastModifiedDateTime"]["value"].replace("Z", "+00:00")),
+            Name=employee_data["Name"]["value"],
+            PaymentInstructionID=employee_data["PaymentInstruction"][0]["id"] if employee_data["PaymentInstruction"] else None,
+            PaymentInstructionRowNumber=employee_data["PaymentInstruction"][0]["rowNumber"] if employee_data["PaymentInstruction"] else None,
+            PaymentInstructionNote=employee_data["PaymentInstruction"][0].get("note"),
+            PaymentInstructionBAccountID=employee_data["PaymentInstruction"][0]["BAccountID"]["value"] if employee_data["PaymentInstruction"] else None,
+            PaymentInstructionDescription=employee_data["PaymentInstruction"][0]["Description"]["value"] if employee_data["PaymentInstruction"] else None,
+            PaymentInstructionInstructionID=employee_data["PaymentInstruction"][0]["InstructionID"]["value"] if employee_data["PaymentInstruction"] else None,
+            PaymentInstructionLocationID=employee_data["PaymentInstruction"][0]["LocationID"]["value"] if employee_data["PaymentInstruction"] else None,
+            PaymentMethod=employee_data["PaymentMethod"]["value"],
+            ReportsToID=employee_data.get("ReportsToID"),
+            SalesAccount=employee_data["SalesAccount"]["value"],
+            SalesSubaccount=employee_data["SalesSubaccount"]["value"],
+            Status=employee_data["Status"]["value"],
+            custom=employee_data.get("custom"),
+            links=employee_data.get("_links")
         )
 
         db.add(employee)
         db.commit()
+        return employee
 
-        return employee_data
-   else:
+    else:
         print(f"Error fetching data for Employee ID {employee_id}: {response.status_code}, {response.json()}")
         raise HTTPException(status_code=response.status_code, detail="Error fetching employee data")
 
